da1f061f0a9ddabb9d6161e0f4c079f4cc2f105b
David Noelte <dnoelte@gmail.com>
2012-04-07 18:27:34 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinionwars::Application.config.secret_token = 'df9e1e93e1dfda44e3a8491e5f9228e9b87dd2d2f97504841b95871fd014a1bc9f6143a33de70fff05860f3280ceabb548f559e5bb2d71849ed0d8bb23654049'
